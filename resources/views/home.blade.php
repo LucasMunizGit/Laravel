f615b0b047568d93ba1278131cbcb915e1ee5bd5
Lucas Muniz <lucaswhitemuniz@outlook.com.br>
2026-09-19 13:19:42 -0300

    feat: configure Tailwind and update homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,17 @@
-<h1>
-    Welcome to the home aloooo    
-</h1>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>
+            {{ config('app.name') }}
+        </title>
 
-
-
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+        <h1 class="text-3xl font-bold underline text-green-500">
+            Hello world!
+        </h1>
+    </body>
+</html>
