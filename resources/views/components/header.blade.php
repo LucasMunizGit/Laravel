714fc4eb16f1c20612778b3c3ce3ea17832ec436
Lucas Muniz <lucaswhitemuniz@outlook.com.br>
2026-09-19 15:45:41 -0300

    feat: add layout, header, and footer components

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,13 @@
+<header class="bg-white border-b-2 flex items-center justify-between p-4">
+    {{-- LOGO --}}
+
+    <div>
+        logo
+    </div>
+
+    {{-- GitHub --}}
+    
+    <div>
+        github
+    </div>
+</header>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>
-            {{ config('app.name') }}
-        </title>
+<x-layout>
+    <main class="py-10">
+        <h1>
+            Veja seus hábitos e metas de forma simples e prática.
+        </h1> 
+    </main>
 
-        @vite('resources/css/app.css')
-    </head>
-    <body>
-        <h1 class="text-3xl font-bold underline text-green-500">
-            Hello world!
-        </h1>
-    </body>
-</html>
+</x-layout>
