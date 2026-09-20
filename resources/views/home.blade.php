bf0bde01e5e7bee7fdd9a39a2267e9ecda0b45b3
Lucas Muniz <lucaswhitemuniz@outlook.com.br>
2026-09-20 02:42:22 -0300

    feat: implement login functionality with authentication and error handling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,8 @@
         <h1>
             Veja seus hábitos e metas de forma simples e prática.
         </h1> 
+        @auth()
+            Bem vindo, {{ auth()->user()->name }}. <a href="/logout">Sair</a>
+        @endauth
     </main>
-
 </x-layout>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,8 +1,44 @@
 <x-layout>
     <main class="py-10">
         <h1>
-            Faça login para acessar seus hábitos e metas.
+            Faça login
         </h1> 
-    </main>
 
+        <section class="mt-4">
+            <form action="/login" method="POST">
+                @csrf
+
+                @error('email')
+                    <p class="text-red-500 text-x1 mt-1">
+                        {{ $message }}
+                    </p>
+                @enderror
+
+                <input
+                    type="email" 
+                    name="email"
+                    placeholder="xablau@exemplo.com"
+                    class="bg-white p-2 border-2"
+                >
+
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="*****************"
+                    class="bg-white p-2 border-2"
+                >
+
+                <button 
+                    type="submit"
+                    class="bg-white p-2 border-2"
+                >
+                    Entra. VAI, Aiinnn... la ele.
+                </button>
+
+            </form>   
+
+        </section>
+
+    </main>
+    
 </x-layout>
